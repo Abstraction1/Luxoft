dfadee870be4df5c8cce9e3e993f3397258762f6
Default <humancollection@gmail.com>
2017-10-27 05:50:10 +0300

    filling in Game.h

--- luxoft_test_solution/luxoft_test_solution/Game.h
+++ luxoft_test_solution/luxoft_test_solution/Game.h
@@ -1,9 +1,31 @@
 #pragma once
+
+#include "Track.h"
+#include "Player.h"
+
 namespace Racing
 {
+	class Player;
+	class Track;
+
 	class Game
 	{
-	private:
 	public:
+		int player_points;
+		int game_speed;
+		Racing::Track * track;
+		Racing::Player * player;
+		
+		enum e_direct { STOP = 0, LEFT, RIGHT, UP, DOWN };
+		e_direct dir;
+
+		Game();
+		void Logic();
+		void Initialization();
+		void Print();
+		void Run();
+		void input();
+		void clearscreen();
+
 	};
 }
